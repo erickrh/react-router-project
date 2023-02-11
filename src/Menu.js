5b3006f399efa8cfd4846cb64d9d44258a597f0b
Erick <123erickrh@gmail.com>
2023-02-11 17:36:30 -0500

    Link & NavLink

--- src/Menu.js
+++ src/Menu.js
@@ -1,9 +1,84 @@
 import React from "react";
+// eslint-disable-next-line no-unused-vars
+import { Link, NavLink } from "react-router-dom";
 
 function Menu() {
   return (
-    <nav>Menu</nav>
+
+    <nav>
+      <ul>
+        
+        {/*
+        With Link
+        <li> 
+          <Link to={"/"}>Home</Link>
+        </li>
+        <li>
+          <Link to={"/blog"}>Blog</Link>
+        </li>
+        <li>
+          <Link to={"/profile"}>Profile</Link>
+        </li> */}
+
+        {/*
+        With NavLink
+        <li>
+          <NavLink // NavLink nos permite obtener el argument isActive, para saber si se esta sobre esa ruta.
+          style={({ isActive }) => ({
+            color: isActive ? 'red' : 'blue', // Si isActive es true, entonces será rojo. De lo contrario será azul.
+          })}
+          to={'/'}
+          >Home</NavLink>
+        </li>
+        <li>
+          <NavLink
+          style={({ isActive }) => ({
+            color: isActive ? 'red' : 'blue',
+            })}
+          to={'/blog'}
+          >Blog</NavLink>
+        </li>
+        <li>
+          <NavLink
+          style={({ isActive }) => ({
+            color: isActive ? 'red' : 'blue',
+          })}
+          to={'/profile'}
+          >Profile</NavLink>
+        </li> */}
+
+        {routes.map(route => (
+          <li>
+            <NavLink
+              style={({ isActive }) => ({
+                color: isActive ? 'red' : 'blue',
+              })}
+              to={route.to}
+            >
+              {route.text}
+            </NavLink>
+          </li>
+        ))}
+        {console.log(routes)}
+      </ul>
+    </nav>
   );
 }
+
+const routes = [];
+  
+routes.push({
+  to: '/',
+  text: 'Home'
+});
+routes.push({
+  to: '/blog',
+  text: 'Blog'
+});
+routes.push({
+  to: '/profile',
+  text: 'Profile'
+});
+
 
 export { Menu };
