1735ebd155331de7a2af272131714ccca9e0d92c
Erick <123erickrh@gmail.com>
2023-02-12 16:27:47 -0500

    useNavigate

--- src/BlogPost.js
+++ src/BlogPost.js
@@ -1,13 +1,19 @@
 import React from "react";
-import { useParams } from "react-router-dom";
+import { useNavigate, useParams } from "react-router-dom";
 import { blogdata } from "./blogdata";
 
 function BlogPost() {
+  const navigate = useNavigate();
   const { slug } = useParams();
+
   const blogpost = blogdata.find(post => post.slug === slug);
+  
+  // Acepta ruta fija (/blog) o dinamica (/blog/:slug), o incluso -1. Puede usarse también para eventos como por ejemplo si alguien no se ha autenticado a los 30 segundos redireccione a otra ruta.
+  const returnToBlog = () => navigate('/blog');
 
   return (
     <>
+      <button onClick={returnToBlog}>Volver al blog</button>
       <h2>{blogpost.title}</h2>
       <code>Author: {blogpost.author}</code>
       <p>{blogpost.content}</p>
